18b59a12d30534b62932153b5f424221f71b94ed
DavidAsmodeus <david.threshold@gmail.com>
2024-09-04 23:03:13 +0200

    Add project files.

---
+++ LibrosCrudClnArch.Application/DI/DI.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace LibrosCrudClnArch.Application.DI
+{
+    public static class DI
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ LibrosCrudClnArch.Infrastructure/DI/DI.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace LibrosCrudClnArch.Infrastructure.DI
+{
+    public static class DI
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
